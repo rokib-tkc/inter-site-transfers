7bfc211270e2f43b296abc1212806527ffdd2022
rokib-tkc <rokib.uddin@tkc.co.uk>
2026-08-04 15:19:36 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Front-end configuration.
+// Update these values after deploying the Google Apps Script web app.
+window.APP_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbxpeOikchlafJAHPgZAyGD-5kCbRWijK1jn4zE1uZQOd1LA-DMJIf6ud3INj2HHW9yj/exec",
+  API_KEY: "Lthdn637H7shdbs8987jnsm72GtsFh",
+  AUTO_REFRESH_MS: 5 * 60 * 1000,
+  JSONP_URL_MAX: 1700,
+  APP_NAME: "Inter-Site Stock Transfers"
+});
